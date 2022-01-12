f613d0a7d50ff5b961a4055102fdd97887743fb8
johj703 <johj703@nate.com>
2022-01-12 11:08:20 +0900

    상세화면

--- app.py
+++ app.py
@@ -177,7 +177,10 @@ def update_like():
     except (jwt.ExpiredSignatureError, jwt.exceptions.DecodeError):
         return redirect(url_for("home"))
 
-
+@app.route('/detail/<keyword>')
+def detail(keyword):
+    # 치킨 메뉴 상세 보기
+    return render_template("detail.html", word=keyword)
 
 if __name__ == '__main__':
     app.run('0.0.0.0', port=5000, debug=True)
